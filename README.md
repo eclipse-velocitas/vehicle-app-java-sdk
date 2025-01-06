81ca44cae3d30617a5dde5959932c2b13fd384f3
Andre Weber <138565883+wba2hi@users.noreply.github.com>
2025-01-06 10:13:04 +0100

    Migrate Vehicle App Java SDK from Kotlin Template Repository (#1)
    * feat: Add Vehicle App Java SDK
    
    * chore: Remove Android SampleApp
    
    * feat: Set source- and targetCompability to JVM 11
    
    * chore: Remove unused Dependencies
    
    * chore: Only use --gradle-user-home for localversion.yml task
    
    * chore: Add Dokka
    
    * docs: Adapt README.md
    
    * chore: Minor Changes

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # vehicle-app-java-sdk
-Vehicle App sdk for Java
+
+Vehicle App Java SDK
+
+This project is in incubation status. Not all required functionality might be migrated yet.
+
+## Overview
+
+The Velocitas Vehicle App Java SDK provides functionality to ease the implementation of Automotive
+Java Applications. 
+
+## Quickstart
+
+Work in Progress
---
+++ buildSrc/build.gradle.kts
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2023 Contributors to the Eclipse Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ */
+
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    `kotlin-dsl`
+    `kotlin-dsl-precompiled-script-plugins`
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget.set(JvmTarget.JVM_11)
+    }
+}
